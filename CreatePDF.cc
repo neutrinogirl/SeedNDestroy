aa5490c8ef7afa35ae67cb50b9ebb42233c41648
P3tru <stephane.zsoldos@gmail.com>
2021-05-11 22:11:07 -0700

    Update CreatePDF

--- CreatePDF.cc
+++ CreatePDF.cc
@@ -5,7 +5,6 @@
 #include <EventDisplay.hh>
 
 #include <iostream>
-#include <numeric>
 
 #include <TH1D.h>
 #include <TH2D.h>
@@ -21,7 +20,6 @@
 
 #include "CreatePDF.hh"
 #include "MathUtils.hh"
-#include "TriggerTimeMap.hh"
 
 int main(int argc, char *argv[]){
 
@@ -46,24 +44,6 @@ int main(int argc, char *argv[]){
 
 
   // ######################################## //
-  // Handle gen shift of ANNIE
-  auto ANNIEShift = [](const TVector3& v){
-    TVector3 vShifted;
-    vShifted.SetX(v.X());
-    vShifted.SetY(-1*(v.Z()-1724));
-    vShifted.SetZ(v.Y()+133.3);
-    return vShifted;
-  };
-  auto ANNIEDirShift = [](const TVector3& v){
-    TVector3 vShifted;
-    vShifted.SetX(v.X());
-    vShifted.SetY(-1*v.Z());
-    vShifted.SetZ(v.Y());
-    return vShifted;
-  };
-
-
-  // ######################################## //
   // Make PDFs for different weight
   std::vector<unsigned int> vPower = {0, 1, 2};
 
@@ -77,121 +57,121 @@ int main(int argc, char *argv[]){
   const zAxis axNHits(1000, 0., 2000.);
 
   TH1D* hNHits = new TH1D("hNHits", "NHits per event ; NHits ; ",
-			  axNHits.nBins, axNHits.min, axNHits.max);
+						  axNHits.nBins, axNHits.min, axNHits.max);
 
   TH1D* hN400 = new TH1D("hN400", "N_{400} per event ; N_{400} ; ",
-			 axNHits.nBins, axNHits.min, axNHits.max);
+						 axNHits.nBins, axNHits.min, axNHits.max);
 
   enum { kTHIT, kTOF };
   std::vector< std::vector<TH2D*> > vvHPDFs;
   vvHPDFs.reserve(vPower.size());
 
   for(const auto& wP : vPower){
-    vvHPDFs.push_back(
-		      {
+	vvHPDFs.push_back(
+		{
 			new TH2D(Form("hCTVSTResPDF_THit_QW%d", wP), "T_{Res} VS Cos(#theta) ; T_{Res} [ns] ; Cos(#theta)",
-				 axTRes.nBins, axTRes.min, axTRes.max,
-				 axCosT.nBins, axCosT.min, axCosT.max),
+					 axTRes.nBins, axTRes.min, axTRes.max,
+					 axCosT.nBins, axCosT.min, axCosT.max),
 			new TH2D(Form("hCTVSTResPDF_TTOF_QW%d", wP), "T_{Res} VS Cos(#theta) ; T_{Res} [ns] ; Cos(#theta)",
-				 axTRes.nBins, axTRes.min, axTRes.max,
-				 axCosT.nBins, axCosT.min, axCosT.max)
-		      }
-		      );
+					 axTRes.nBins, axTRes.min, axTRes.max,
+					 axCosT.nBins, axCosT.min, axCosT.max)
+		}
+	);
   }
 
   // ######################################## //
   // DET Boundaries
   bnds *b;
   if(args.isBox){
-    b = new BoxBnds( { args.bnds[0] , args.bnds[1] , args.bnds[2] });
+	b = new BoxBnds( { args.bnds[0] , args.bnds[1] , args.bnds[2] });
   } else{
-    b = new CylBnds(args.bnds[0], args.bnds[1]);
+	b = new CylBnds(args.bnds[0], args.bnds[1]);
   }
   if(args.isVerbose)
-    b->Print();
+	b->Print();
 
   const double MaxDWall = b->GetMaxDWall();
   const double ScaleDWall = 1.5;
 
   auto hDWallVSTTime = new TH2D("hDWallVSTTime", "TRUE d_{Wall} vs T_{Trig} ; T_{Trig} [ns] ; d_{Wall} [mm]",
-				20, b->vT.min, b->vT.max,
-				20*ScaleDWall, 0., MaxDWall*ScaleDWall);
+								20, b->vT.min, b->vT.max,
+								20*ScaleDWall, 0., MaxDWall*ScaleDWall);
 
-  
+
   // ######################################## //
   // Loop and get vector of NHits
   ProgressBar progress_bar(nEvts, 70);
   for(auto iEvt=0; iEvt<nEvts; iEvt++){
 
-    // Record the tick
-    ++progress_bar;
+	// Record the tick
+	++progress_bar;
 
-    // Point to evt
-    w_rat.SetEvt(iEvt);
+	// Point to evt
+	w_rat.SetEvt(iEvt);
 
-    // Get number of trigger associated with an event
-    // i.e, number of EV inside the rat DS
-    auto nTriggers = w_rat.GetNTriggers();
+	// Get number of trigger associated with an event
+	// i.e, number of EV inside the rat DS
+	auto nTriggers = w_rat.GetNTriggers();
 
-    // Assume only single particle are generated
-    const int iParticle = 0;
+	// Assume only single particle are generated
+	const int iParticle = 0;
 
-    for(auto iTrigger=0; iTrigger<nTriggers; iTrigger++){
+	for(auto iTrigger=0; iTrigger<nTriggers; iTrigger++){
 
-      double TrigTime = 0;
+	  const auto TrigTime = w_rat.GetTriggerTime(iTrigger);
 
-      // Get True info to build PDFs
-      const auto PosTrue = ANNIEShift(w_rat.GetPosTrue(iParticle));
-      const auto DirTrue = ANNIEDirShift(w_rat.GetDirTrue(iParticle));
-      const auto TTrue = w_rat.GetTTrue(iParticle);
+	  // Get True info to build PDFs
+	  const auto PosTrue = w_rat.GetPosTrue(iParticle);
+	  const auto DirTrue = w_rat.GetDirTrue(iParticle);
+	  const auto TTrue = w_rat.GetTTrue(iParticle);
 
-      // Get vector of hits
-      std::vector<Hit> vHits = w_rat.GetVHits(iTrigger);
-      if(vHits.empty())
-	continue;
-      ReTriggerVHits(vHits, 2., TrigTime);
+	  // Get vector of hits
+	  std::vector<Hit> vHits = w_rat.GetVHits(iTrigger);
+	  if(vHits.empty())
+		continue;
+	  std::sort(vHits.begin(), vHits.end());
 
-      //
-      // DO STUFF
-      //
+	  //
+	  // DO STUFF
+	  //
 
-      // GetNHits per evt
-      hN400->Fill(GetNPrompts(vHits, 400));
-      hNHits->Fill(GetNPrompts(vHits));
+	  // GetNHits per evt
+	  hN400->Fill(GetNPrompts(vHits, 400));
+	  hNHits->Fill(GetNPrompts(vHits));
 
-      for(auto iPower = 0; iPower<vPower.size(); iPower++){
+	  for(auto iPower = 0; iPower<vPower.size(); iPower++){
 
-	// Get PDF
-	for(auto& hit: vHits){
+		// Get PDF
+		for(auto& hit: vHits){
 
-	  const double QW = fweight(hit, vPower[iPower]);
+		  const double QW = fweight(hit, vPower[iPower]);
 
-	  vvHPDFs[iPower][kTHIT]->Fill(hit.GetTRes(PosTrue, TTrue),
-				       hit.GetCosTheta(PosTrue, DirTrue),
-				       QW);
-	  vvHPDFs[iPower][kTOF]->Fill(hit.GetTRes(PosTrue, TTrue-TrigTime),
-				      hit.GetCosTheta(PosTrue, DirTrue),
-				      QW);
+		  vvHPDFs[iPower][kTHIT]->Fill(hit.GetTRes(PosTrue, TTrue),
+									   hit.GetCosTheta(PosTrue, DirTrue),
+									   QW);
+		  vvHPDFs[iPower][kTOF]->Fill(hit.GetTRes(PosTrue, TTrue-TrigTime),
+									  hit.GetCosTheta(PosTrue, DirTrue),
+									  QW);
+
+		}
+
+	  }
+
+
+	  double dWall = b->GetDWall(PosTrue);
+	  hDWallVSTTime->Fill(TrigTime, dWall);
+
+	  // ...
 
 	}
 
-      }
-
-
-      double dWall = b->GetDWall(PosTrue);
-      hDWallVSTTime->Fill(TrigTime, dWall);
-
-      // ...
-
-    }
-
-    if(isVerbose)
-      progress_bar.display();
+	if(isVerbose)
+	  progress_bar.display();
 
   }
 
   if(isVerbose)
-    progress_bar.done();
+	progress_bar.done();
 
   // #################################### //
   // #### #### #### FINISH #### #### #### //
@@ -211,15 +191,15 @@ int main(int argc, char *argv[]){
 
   for(auto& vHPDFs:vvHPDFs){
 
-    ScaleHist(vHPDFs[kTHIT], static_cast<double>(nEvts));
-    vHPDFs[kTHIT]->Write();
-    vHPDFs[kTHIT]->ProjectionX()->Write();
-    vHPDFs[kTHIT]->ProjectionY()->Write();
+	ScaleHist(vHPDFs[kTHIT], static_cast<double>(nEvts));
+	vHPDFs[kTHIT]->Write();
+	vHPDFs[kTHIT]->ProjectionX()->Write();
+	vHPDFs[kTHIT]->ProjectionY()->Write();
 
-    ScaleHist(vHPDFs[kTOF], static_cast<double>(nEvts));
-    vHPDFs[kTOF]->Write();
-    vHPDFs[kTOF]->ProjectionX()->Write();
-    vHPDFs[kTOF]->ProjectionY()->Write();
+	ScaleHist(vHPDFs[kTOF], static_cast<double>(nEvts));
+	vHPDFs[kTOF]->Write();
+	vHPDFs[kTOF]->ProjectionX()->Write();
+	vHPDFs[kTOF]->ProjectionY()->Write();
 
   }
 
