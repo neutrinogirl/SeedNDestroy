e309bfe49d49f351de9777c8e25576b618244d81
Stephane Zsoldos <stephane.zsoldos@gmail.com>
2022-07-10 22:10:37 -0700

    change bins

--- src/SnD/ERec/ERecAnalysis.cc
+++ src/SnD/ERec/ERecAnalysis.cc
@@ -38,15 +38,44 @@ void ERecAnalysis::Do(void *Data) {
 
 }
 #include <TFile.h>
+#include <TFitResult.h>
+#include <TFitResultPtr.h>
+#include <TGraphErrors.h>
+#include <TF1.h>
+#include <TF2.h>
+
+// Calculate the error of a ratio of two double
+static double GetErr(double X, double Y){
+  return std::sqrt(X)/Y + X*std::sqrt(Y)/std::pow(Y, 2);
+}
+
 void ERecAnalysis::Export(const char *filename) {
 
-  TFile *f = new TFile(filename, "RECREATE");
-  for(const auto& h: v2D) {
-	h->Write();
-  }
-  for(const auto& h: v2DWall) {
-	h->Write();
-  }
-  f->Close();
+  TGraphErrors gNHits;
+  gNHits.SetLineWidth(2);
+  gNHits.SetMarkerSize(2);
+  gNHits.SetMarkerStyle(kPlus);
+  gNHits.SetMarkerColor(kBlue-4);
+  gNHits.SetLineColor(kBlue-4);
+  TGraphErrors gQ;
+  gQ.SetLineWidth(2);
+  gQ.SetMarkerSize(2);
+  gQ.SetMarkerStyle(kPlus);
+  gQ.SetMarkerColor(kBlue-4);
+  gQ.SetLineColor(kBlue-4);
 
+  TFile *f = new TFile(filename, "RECREATE");
+  for(auto i=0; i<vEdges.size(); i++){
+	v2D[i]->Write();
+	v2DWall[i]->Write();
+	TFitResultPtr r = v2D[i]->ProjectionX()->Fit("gaus", "SQ");
+	gNHits.SetPoint(gNHits.GetN(), vEdges[i], r->Parameter(2)/r->Parameter(1));
+	gNHits.SetPointError(gNHits.GetN()-1, 0., GetErr(r->Parameter(2), r->Parameter(1)));
+	r = v2D[i]->ProjectionY()->Fit("gaus", "SQ");
+	gQ.SetPoint(gQ.GetN(), vEdges[i], r->Parameter(2)/r->Parameter(1));
+	gQ.SetPointError(gQ.GetN()-1, 0., GetErr(r->Parameter(2), r->Parameter(1)));
+  }
+  gNHits.Write();
+  gQ.Write();
+  f->Close();
 }
