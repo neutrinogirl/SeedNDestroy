6d670878fade132907b41be9a6b3fdc7d4135cbe
Stephane Zsoldos <stephane.zsoldos@gmail.com>
2022-07-10 19:57:14 -0700

    change bins

--- src/SnD/ERec/ERecAnalysis.cc
+++ src/SnD/ERec/ERecAnalysis.cc
@@ -8,7 +8,7 @@ ERecAnalysis::ERecAnalysis() {
   vEdges = {0., 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0};
 
   for(const auto& E: vEdges){
-	v2D.push_back(new TH2D(Form("h2D_E%.1f", E), Form("E=%.1f ; N_{hits} ; Q ", E), 100, 0., 10000., 100, 0., 10000.));
+	v2D.push_back(new TH2D(Form("h2D_E%.1f", E), Form("E=%.1f ; N_{hits} ; Q ", E), 20, 0., 2000., 20, 0., 2000.));
   }
 
 
